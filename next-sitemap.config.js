15781968012699c0dd2466ed4a0fa8e5f860157d
Seda <seda.diriker@gmail.com>
2025-08-28 13:14:02 +0300

    fixed

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://oyun-portali.vercel.app', 
+  siteUrl: 'https://oyun-portali.vercel.app', // Sitenizin doğru URL'sini buraya girin
   generateRobotsTxt: true, // robots.txt dosyasını otomatik oluşturur
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.cdn.famobi.com",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
