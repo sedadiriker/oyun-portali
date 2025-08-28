21c665b981f063d49cc9eaf1e3ede636f221c818
Seda <seda.diriker@gmail.com>
2025-08-28 13:35:18 +0300

    fixed

--- src/app/components/clientGrid.js
+++ src/app/components/clientGrid.js
@@ -4,9 +4,12 @@
 import { useState, useMemo } from 'react';
 import Header from "./header";
 import GameGrid from "./gameGrid";
+import Image from "next/image";
 
 export default function ClientGrid({ games, categories }) {
   const [selectedCategory, setSelectedCategory] = useState("");
+
+   const featuredGames = [...games].sort(() => 0.5 - Math.random()).slice(0, 5);
 
   const filteredGames = useMemo(() => {
     if (!selectedCategory) {
@@ -22,6 +25,34 @@ export default function ClientGrid({ games, categories }) {
         setCategory={setSelectedCategory}
         categories={categories}
       />
+         <div className="flex justify-center">
+        {featuredGames.length > 0 && (
+          <section className="max-w-7xl mx-auto p-4">
+            <h2 className="text-xl font-bold mb-2">Öne Çıkan Oyunlar</h2>
+            <div className="flex overflow-x-auto gap-4 py-2">
+              {featuredGames.map((game) => (
+                <div
+                  key={game.package_id}
+                  className="min-w-[220px] bg-[#111827] rounded-lg shadow-lg cursor-pointer hover:scale-[1.05] transition flex-shrink-0"
+                >
+                  <Image
+                    src={game.thumb}
+                    alt={game.name}
+                    width={220}
+                    height={120}
+                    className="w-full h-[120px] object-cover rounded-t-lg"
+                  />
+                  <div className="p-2">
+                    <p className="font-semibold text-sm truncate">
+                      {game.name}
+                    </p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </section>
+        )}
+      </div>
       <GameGrid games={filteredGames} />
     </>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,7 @@
-// app/page.js (Güncellenmiş Sunucu Bileşeni)
-import Header from "./components/header";
 import Footer from "./components/footer";
-import Image from "next/image";
 import ClientGrid from "./components/clientGrid";
 
 
-// Veriyi sunucu tarafında çeken asenkron fonksiyon
 async function getGames() {
   const res = await fetch("https://oyun-portali.vercel.app/api/famobi", {
     cache: "no-store",
@@ -30,40 +26,13 @@ export default async function Home() {
     );
   }
 
-  const featuredGames = [...games].sort(() => 0.5 - Math.random()).slice(0, 5);
+ 
   const categories = [...new Set(games.flatMap((g) => g.categories))];
 
   return (
     <div className="bg-gradient-to-b from-[#0b1220] to-[#0f172a] text-gray-200 min-h-screen text-center">
 
-      <div className="flex justify-center">
-        {featuredGames.length > 0 && (
-          <section className="max-w-7xl mx-auto p-4">
-            <h2 className="text-xl font-bold mb-2">Öne Çıkan Oyunlar</h2>
-            <div className="flex overflow-x-auto gap-4 py-2">
-              {featuredGames.map((game) => (
-                <div
-                  key={game.package_id}
-                  className="min-w-[220px] bg-[#111827] rounded-lg shadow-lg cursor-pointer hover:scale-[1.05] transition flex-shrink-0"
-                >
-                  <Image
-                    src={game.thumb}
-                    alt={game.name}
-                    width={220}
-                    height={120}
-                    className="w-full h-[120px] object-cover rounded-t-lg"
-                  />
-                  <div className="p-2">
-                    <p className="font-semibold text-sm truncate">
-                      {game.name}
-                    </p>
-                  </div>
-                </div>
-              ))}
-            </div>
-          </section>
-        )}
-      </div>
+   
 
       <ClientGrid games={games} categories={categories} />
 
